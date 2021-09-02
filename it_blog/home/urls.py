716329156e7eea6406b16bb20849c3eeea8dfea8
unknown <amelyanchuk.i@gmail.com>
2021-09-02 22:10:12 +0300

    home page

--- it_blog/home/urls.py
+++ it_blog/home/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from home.views import home_page
+
 
 urlpatterns = [
     path("", home_page, name="home_page")
 ]
+
+
